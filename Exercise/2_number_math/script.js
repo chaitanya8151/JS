917800d0567fbeb4eabc6375e62cd9f50d66e373
Chaitanya <chaitanyayadwad8@gmail.com>
2024-08-01 23:07:03 +0530

    learn js basics

---
+++ Exercise/2_number_math/script.js
@@ -0,0 +1,17 @@
+console.log(10+3*8+5); 
+
+console.log((10+3*8+5)/3);
+console.log((18.50+7.50*2)*0.1);
+console.log((18.50+7.50*2)*0.2);
+
+//Math.floor()
+console.log(Math.floor(2,8)); //2
+//Math.ceil()
+console.log(Math.ceil(2.2));//3
+
+//from celsium to fahrenheit
+console.log((25*1.8)+32);
+//from fahrenheit to celsius
+console.log((86-32)*(5/9));
+
+
---
+++ Exercise/3_strings/script.js
@@ -0,0 +1,5 @@
+console.log('my name is: chaitanya');
+console.log('my name is: '+'chaitanya');
+console.log('total cost: $'+(5+3));
+console.log(`total cost: $${5+3}`);
+alert(`total cost: $${5+3}`);
